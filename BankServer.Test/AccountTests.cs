52e6bde291389d413cd75b25bec566e2ee86b313
Assaf Tzur-El <28855903+AssafTzurEl@users.noreply.github.com>
2022-07-05 17:08:07 +0300

    Add to tests: DataRow attribute, FluentAssertions

--- BankServer.Test/AccountTests.cs
+++ BankServer.Test/AccountTests.cs
@@ -1,4 +1,6 @@
 using BankServer.Model;
+using FluentAssertions;
+
 
 namespace BankServer.Test
 {
@@ -10,52 +12,69 @@ namespace BankServer.Test
         {
             Account sut = new Account();
 
-            Assert.AreEqual(0, sut.Balance);
+            //Assert.AreEqual(0, sut.Balance);
+            sut.Balance.Should().Be(0);
         }
 
         [TestMethod]
-        public void When_CreditPositiveAmount_Expect_NewBalance()
+        [DataRow(0.0)]
+        [DataRow(1.0)]
+        [DataRow(100.0)]
+        [DataRow(1000000000.0)]
+        [DataRow(3.1415926)]
+        public void When_CreditPositiveAmount_Expect_NewBalance(double amount)
         {
-            const decimal amount = 100;
-
+            decimal decimalAmount = (decimal) amount;
             Account sut = new Account();
 
-            sut.Credit(amount);
+            sut.Credit(decimalAmount);
 
-            Assert.AreEqual(amount, sut.Balance);
+            //Assert.AreEqual(decimalAmount, sut.Balance);
+            sut.Balance.Should().Be(decimalAmount);
         }
 
         [TestMethod]
         public void When_CreditNegativeAmount_Expect_OutOfRangeException()
         {
-            const decimal amount = -100;
+            const decimal amount = -100m;
 
             Account sut = new Account();
 
-            Assert.ThrowsException<ArgumentOutOfRangeException>(() => sut.Credit(amount));
+            //Assert.ThrowsException<ArgumentOutOfRangeException>(() => sut.Credit(amount));
+            var action = () => sut.Credit(amount);
+
+            action.Should().Throw<ArgumentOutOfRangeException>();
         }
 
         [TestMethod]
-        public void When_ChargePositiveAmount_Expect_NewBalance()
+        [DataRow(0.0)]
+        [DataRow(1.0)]
+        [DataRow(100.0)]
+        [DataRow(1000000000.0)]
+        [DataRow(3.1415926)]
+        public void When_ChargePositiveAmount_Expect_NewBalance(double amount)
         {
-            const decimal amount = 100;
-
+            decimal decimalAmount = (decimal) amount;
             Account sut = new Account();
 
-            sut.Charge(amount);
+            sut.Charge(decimalAmount);
 
-            Assert.AreEqual(-amount, sut.Balance);
+            //Assert.AreEqual(-decimalAmount, sut.Balance);
+            sut.Balance.Should().Be(-decimalAmount);
         }
 
 
         [TestMethod]
         public void When_ChargeNegativeAmount_Expect_OutOfRangeException()
         {
-            const decimal amount = -100;
+            const decimal amount = -100m;
 
             Account sut = new Account();
 
-            Assert.ThrowsException<ArgumentOutOfRangeException>(() => sut.Charge(amount));
+            //Assert.ThrowsException<ArgumentOutOfRangeException>(() => sut.Charge(amount));
+            var action = () => sut.Charge(amount);
+
+            action.Should().Throw<ArgumentOutOfRangeException>();
         }
     }
 }
