5d5abbb5c04a2fde4375eef1bc025b57e173beb5
Assaf Tzur-El <28855903+AssafTzurEl@users.noreply.github.com>
2022-07-05 18:43:29 +0300

    Add tests for in-memory repository

---
+++ BankServer.Test/InMemoryAccountRepositoryTests.cs
@@ -0,0 +1,55 @@
+﻿using BankServer.Model;
+using BankServer.Repositories;
+using FluentAssertions;
+
+namespace BankServer.Test
+{
+    [TestClass]
+    public class InMemoryAccountRepositoryTests
+    {
+        [TestMethod]
+        public void When_NoAccountAdded_Expect_GetToReturnZeroItems()
+        {
+            var sut = new InMemoryAccountRepository();
+
+            sut.GetAll().Should().BeEmpty();
+        }
+
+        [TestMethod]
+        public void When_AddingAnAccount_Expect_ReturnedValueToMatchAccount()
+        {
+            const string AccountName = "Assaf";
+
+            var sut = new InMemoryAccountRepository();
+            var account = new Account
+            {
+                Name = AccountName
+                // ID = 0 by default
+            };
+
+            var actual = sut.Add(account);
+
+            actual.Name.Should().Be(AccountName);
+            actual.Id.Should().NotBe(0);
+            actual.Balance.Should().Be(0);
+        }
+
+        [TestMethod]
+        [DataRow(1)]
+        [DataRow(-1)]
+        public void When_AddingAnAccountWithNonZeroId_Expect_ArgumentException(int id)
+        {
+            var sut = new InMemoryAccountRepository();
+            var account = new Account
+            {
+                Id = id
+            };
+
+            var action = () => sut.Add(account);
+
+            action.Should().Throw<ArgumentException>().WithMessage("New account must not have an ID");
+        }
+
+        // Many more tests should be added...
+    }
+}
